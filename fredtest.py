68569537c12857d190296665b9924d99f2ad3014
Tyler Denniston <denniston.t@gmail.com>
2011-02-23 18:20:21 +0000

    - Add a test for gdb reverse-watch to fredtest.py. Reminder: to run tests,   simply execute './fredtest.py'.
    git-svn-id: svn://joust.ccs.neu.edu/home/tyler/svnroot/pthread-wrappers/fred@164 679428e0-fc1f-4e08-a6f4-36083810378a

--- fredtest.py
+++ fredtest.py
@@ -34,24 +34,9 @@ This file should be executable from the command line to run several integration 
 GS_PASSED_STRING = "Passed"
 GS_FAILED_STRING = "Failed"
 
-# gd_test_results format is:
-# key: name of test
-# value: result string: one of "Passed", "Failed"
-gd_test_results = {}
+# Used for storing variable values between runs.
 gd_stored_variables = {}
 g_debugger = None
-
-def run_all_tests():
-    """Run all available test suites."""
-    print "%-30s | %-15s" % ("Test name", "Result")
-    print "-" * 31 + "+" + "-" * 15
-    # TODO: This is hackish.
-    fred.fredio.gb_hide_output = True
-    run_integration_tests()
-    run_unit_tests()
-
-def print_aggregate_test_results():
-    """Print a tabulized summary of test results."""
 
 def start_session(l_cmd):
     """Start the given command line as a fred session."""
@@ -79,16 +64,19 @@ def check_stored_variable(s_name):
     global gd_stored_variables, g_debugger
     return gd_stored_variables[s_name] == g_debugger.evaluate_expression(s_name)
 
-def run_integration_tests():
-    """Run all available integration tests."""
-    gdb_record_replay(2)
-    
+def check_variable(s_name, s_value):
+    """Evaluate given variable in debugger and check against given value."""
+    return s_value == str(g_debugger.evaluate_expression(s_name))
+
+def print_test_name(s_name):
+    print "%-30s | " % s_name,
+    sys.stdout.flush()
+
 def gdb_record_replay(n_count=1):
-    global gd_test_results
+    """Run a test on deterministic record/replay on pthread-test example."""
     l_cmd = ["gdb", "../test-programs/pthread-test"]
     for i in range(0, n_count):
-        print "%-30s | " % ("gdb record/replay " + str(i)),
-        sys.stdout.flush()
+        print_test_name("gdb record/replay %d" % i)
         start_session(l_cmd)
         execute_commands(["b main", "r", "fred-ckpt", "c"])
         store_variable("solution")
@@ -99,9 +87,41 @@ def gdb_record_replay(n_count=1):
             print GS_FAILED_STRING
         end_session()
 
+def gdb_reverse_watch(n_count=1):
+    """Run a reverse-watch test on test_list linked list example."""
+    l_cmd = ["gdb", "../test-programs/test_list"]
+    for i in range(0, n_count):
+        print_test_name("gdb reverse watch %d" % i)
+        start_session(l_cmd)
+        execute_commands(["b main", "r", "fred-ckpt", "b 26",
+                          "c", "fred-rw list_len(head) < 10"])
+        if check_variable("list_len(head)", "9"):
+            execute_commands(["n"])
+            if check_variable("list_len(head)", "10"):
+                print GS_PASSED_STRING
+            else:
+                print GS_FAILED_STRING
+        else:
+            print GS_FAILED_STRING
+        end_session()
+        
+def run_integration_tests():
+    """Run all available integration tests."""
+    gdb_record_replay()
+    gdb_reverse_watch()
+    
 def run_unit_tests():
     """Run all available unit tests."""
     pass
+
+def run_all_tests():
+    """Run all available test suites."""
+    print "%-30s | %-15s" % ("Test name", "Result")
+    print "-" * 31 + "+" + "-" * 15
+    # TODO: This is hackish. Used to hide fred_info() messages.
+    fred.fredio.gb_hide_output = True
+    run_integration_tests()
+    run_unit_tests()
 
 if __name__ == "__main__":
     run_all_tests()
