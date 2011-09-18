d2490205669451641f041bc93abe5e3563bb6a2f
Tyler Denniston <denniston.t@gmail.com>
2011-09-18 15:00:18 +0000

    - Adding test program 'many-threads' which currently fails to replay due to   some jalloc locking bug. Will add to make check when that bug is fixed.
    git-svn-id: svn://joust.ccs.neu.edu/home/tyler/svnroot/pthread-wrappers/fred@477 679428e0-fc1f-4e08-a6f4-36083810378a

---
+++ fredtest/test-programs/many-threads.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 250
+
+void *worker(void *arg)
+{
+  long i = (long)arg;
+  printf("Thread %ld alive\n", i);
+  usleep(100);
+  printf("aaaaand thread %ld exiting.\n", i);
+}
+
+int main()
+{
+  pthread_t threads[NUM_THREADS];
+  long i = 0;
+  int rc = 0;
+  for (i = 0; i < NUM_THREADS; i++) {
+    rc = pthread_create(&threads[i], NULL, worker, (void *)i);
+    if (rc) {
+      perror("pthread_create");
+      return 1;
+    }
+    rc = pthread_join(threads[i], NULL);
+    if (rc) {
+      perror("pthread_join");
+      return 1;
+    }
+  }
+
+  return 0;
+}
